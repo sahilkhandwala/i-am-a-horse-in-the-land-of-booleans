6dfb665a72a1b6dc353ee500a590274a20d2bba0
Sahil Khandwala <sahilkhandwala@Sahils-MBP.local>
2018-09-07 22:09:32 -0700

    Set 2

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,32 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x] (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn abs [x] (if (< x 0) (* x -1) x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? [divisor n] (if (== (mod n divisor) 0) true false ))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [n] (cond
+       (divides? 15 n) "gotcha!"
+       (divides? 3 n)  "fizz"
+       (divides? 5 n) "buzz"
+       :else         ""))
 
-(defn teen? [age]
-  ":(")
+
+(defn teen? [age] (<= 13 age 19))
 
 (defn not-teen? [age]
   ":(")
 
-(defn generic-doublificate [x]
-  ":(")
+(defn generic-doublificate [x]  (cond
+                                (number? x) (* x 2)
+                                (empty? x) nil
+                                (or (list? x) (vector? x)) (* (count x) 2)
+                                :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (or (and (== (mod year 4) 0) (and (==(mod year 100) 0) (==(mod year 400) 0)))
+  (and (== (mod year 4) 0) (not (== (mod year 100) 0)))))
 
 ; '_______'
